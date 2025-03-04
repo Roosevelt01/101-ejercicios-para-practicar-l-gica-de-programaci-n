ec47326d6d481bad07f87a5e8942ae09813a68d9
Roosevelt01 <94496123+Roosevelt01@users.noreply.github.com>
2025-03-03 21:44:10 -0400

    Create 2 - suma_tresCalificaciones.java

---
+++ 2 - suma_tresCalificaciones.java
@@ -0,0 +1,17 @@
+/*
+programa que calcule la suma de tres calificaciones utilizando POO, interfaces y lambdas en Java, te propongo el siguiente ejemplo. 
+Este programa define una interfaz funcional para calcular la suma de las calificaciones y utiliza expresiones lambda para implementar esta interfaz.
+*/
+
+//Interfa
+package org.jason.practice.suma;
+
+public interface CalcularSumar {
+    double calcularSum(double num1, double num2, double num3);
+}
+
+//Main principal
+
+
+//Ejemplo #2
+
